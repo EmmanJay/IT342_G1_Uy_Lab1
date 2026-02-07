a637434899d1945bac86922feb10cbacff4ea4ad
EmmanJay <emmanjayuy@gmail.com>
2026-02-07 09:27:15 +0800

    setup(backend): initialize spring boot project

---
+++ backend/src/test/java/com/sysinteg/uy/UyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysinteg.uy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
